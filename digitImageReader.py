d754663e849af3bd062b2d99f8579d829e4df1ea
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-12-01 13:29:40 +0000

    Updated index.htnl and added code to python file

--- digitImageReader.py
+++ digitImageReader.py
@@ -1,5 +1,12 @@
+# Code Adapted from - https://github.com/sleepokay/mnist-flask-app/blob/master/app.py
+
 # Imports
 from flask import Flask, render_template, request
+from scipy.misc import imsave, imread, imresize
+import numpy as np
+import keras.models
+import re
+import base64
 
 
 app = Flask(__name__)
@@ -7,6 +14,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/predict/', methods=['GET','POST'])
+def predict():
+    # get data from drawing canvas and save as image
+    parseImage(request.get_data())
+
+    # read parsed image back in 8-bit, black and white mode (L)
+    x = imread('output.png', mode='L')
+    x = np.invert(x)
+    x = imresize(x,(28,28))
+
+    # reshape image data for use in neural network
+    x = x.reshape(1,28,28,1)
+
+    # load model to predict number
+    model = keras.models.load_model("mnist_model.h5")
+    out = model.predict(x)
+    print(out)
+    print(np.argmax(out, axis=1))
+    response = np.array_str(np.argmax(out, axis=1))
+    return response 
+
+def parseImage(imgData):
+    # parse canvas bytes and save as output.png
+    imgstr = re.search(b'base64,(.*)', imgData).group(1)
+    with open('output.png','wb') as output:
+        output.write(base64.decodebytes(imgstr))
 
 if __name__ == '__main__':
     app.run(debug = True)
